67354384eea96c88a5b633d605ced6af1375de4b
Dimitri Berthault <dimitri.berthault@quantumsimulations.de>
2024-07-30 14:12:38 +0200

    byebye windows

--- tests/test_qoqo_tket.py
+++ tests/test_qoqo_tket.py
@@ -127,48 +127,48 @@ def test_run_qoqo_tket() -> None:
     assert np.isclose(results[2]["statevector"][0], state_res, atol=1e-5).all()
 
 
-# def test_run_complex_qoqo_tket() -> None:
-#     """Test compiling with qoqo_tket."""
-#     circuit = Circuit()
-#     circuit += ops.Hadamard(0)
-#     circuit += ops.CNOT(0, 1)
-#     circuit += ops.PauliX(1)
-#     circuit += ops.CNOT(1, 2)
-#     circuit += ops.PauliZ(2)
-#     circuit += ops.PragmaGetStateVector("statevector", Circuit())
-#     state_res = [0, 0, 0, -1 / np.sqrt(2), 1 / np.sqrt(2), 0, 0, 0]
+def test_run_complex_qoqo_tket() -> None:
+    """Test compiling with qoqo_tket."""
+    circuit = Circuit()
+    circuit += ops.Hadamard(0)
+    circuit += ops.CNOT(0, 1)
+    circuit += ops.PauliX(1)
+    circuit += ops.CNOT(1, 2)
+    circuit += ops.PauliZ(2)
+    circuit += ops.PragmaGetStateVector("statevector", Circuit())
+    state_res = [0, 0, 0, -1 / np.sqrt(2), 1 / np.sqrt(2), 0, 0, 0]
 
-#     backend = ProjectQBackend()
-#     tket_backend = QoqoTketBackend(backend)
-#     results = tket_backend.run_circuit(circuit)
-#     assert np.isclose(results[2]["statevector"][0], state_res, atol=1e-5).all()
+    backend = ProjectQBackend()
+    tket_backend = QoqoTketBackend(backend)
+    results = tket_backend.run_circuit(circuit)
+    assert np.isclose(results[2]["statevector"][0], state_res, atol=1e-5).all()
 
 
-# def test_run_multiple_qoqo_tket() -> None:
-#     """Test compiling with qoqo_tket."""
-#     circuit = Circuit()
-#     circuit += ops.PauliX(0)
-#     circuit += ops.PragmaGetStateVector("psi", Circuit())
+def test_run_multiple_qoqo_tket() -> None:
+    """Test compiling with qoqo_tket."""
+    circuit = Circuit()
+    circuit += ops.PauliX(0)
+    circuit += ops.PragmaGetStateVector("psi", Circuit())
 
-#     state_res = [0, 1]
+    state_res = [0, 1]
 
-#     circuit_2 = Circuit()
-#     circuit_2 += ops.Hadamard(0)
-#     circuit_2 += ops.CNOT(0, 1)
-#     circuit_2 += ops.PauliX(1)
-#     circuit_2 += ops.CNOT(1, 2)
-#     circuit_2 += ops.PauliZ(2)
-#     circuit_2 += ops.PragmaGetStateVector("psi2", Circuit())
+    circuit_2 = Circuit()
+    circuit_2 += ops.Hadamard(0)
+    circuit_2 += ops.CNOT(0, 1)
+    circuit_2 += ops.PauliX(1)
+    circuit_2 += ops.CNOT(1, 2)
+    circuit_2 += ops.PauliZ(2)
+    circuit_2 += ops.PragmaGetStateVector("psi2", Circuit())
 
-#     state_res_2 = [0, 0, 0, -1 / np.sqrt(2), 1 / np.sqrt(2), 0, 0, 0]
+    state_res_2 = [0, 0, 0, -1 / np.sqrt(2), 1 / np.sqrt(2), 0, 0, 0]
 
-#     backend = ProjectQBackend()
-#     tket_backend = QoqoTketBackend(backend)
-#     results = tket_backend.run_circuit([circuit, circuit_2])
-#     assert (
-#         np.isclose(results[0][2]["psi"][0], state_res, atol=1e-5).all()
-#         and np.isclose(results[1][2]["psi2"][0], state_res_2, atol=1e-5).all()
-#     )
+    backend = ProjectQBackend()
+    tket_backend = QoqoTketBackend(backend)
+    results = tket_backend.run_circuit([circuit, circuit_2])
+    assert (
+        np.isclose(results[0][2]["psi"][0], state_res, atol=1e-5).all()
+        and np.isclose(results[1][2]["psi2"][0], state_res_2, atol=1e-5).all()
+    )
 
 
 def assert_quantum_program_equal(
@@ -353,101 +353,101 @@ def test_quantum_program_with_constant_circuit() -> None:
     assert_quantum_program_equal(transpiled_program, quantum_program_res)
 
 
-# def test_run_program() -> None:
-#     """Test QoqoTketBackend.run_program method."""
-#     backend = QoqoTketBackend()
+def test_run_program() -> None:
+    """Test QoqoTketBackend.run_program method."""
+    backend = QoqoTketBackend()
 
-#     init_circuit = Circuit()
-#     init_circuit += ops.RotateX(0, "angle_0")
-#     init_circuit += ops.RotateY(0, "angle_1")
+    init_circuit = Circuit()
+    init_circuit += ops.RotateX(0, "angle_0")
+    init_circuit += ops.RotateY(0, "angle_1")
 
-#     z_circuit = Circuit()
-#     z_circuit += ops.DefinitionBit("ro_z", 1, is_output=True)
-#     z_circuit += ops.PragmaRepeatedMeasurement("ro_z", 1000, None)
+    z_circuit = Circuit()
+    z_circuit += ops.DefinitionBit("ro_z", 1, is_output=True)
+    z_circuit += ops.PragmaRepeatedMeasurement("ro_z", 1000, None)
 
-#     x_circuit = Circuit()
-#     x_circuit += ops.DefinitionBit("ro_x", 1, is_output=True)
-#     x_circuit += ops.Hadamard(0)
-#     x_circuit += ops.PragmaRepeatedMeasurement("ro_x", 1000, None)
+    x_circuit = Circuit()
+    x_circuit += ops.DefinitionBit("ro_x", 1, is_output=True)
+    x_circuit += ops.Hadamard(0)
+    x_circuit += ops.PragmaRepeatedMeasurement("ro_x", 1000, None)
 
-#     measurement_input = PauliZProductInput(1, False)
-#     z_basis_index = measurement_input.add_pauliz_product(
-#         "ro_z",
-#         [
-#             0,
-#         ],
-#     )
-#     x_basis_index = measurement_input.add_pauliz_product(
-#         "ro_x",
-#         [
-#             0,
-#         ],
-#     )
-#     measurement_input.add_linear_exp_val(
-#         "<H>",
-#         {x_basis_index: 0.1, z_basis_index: 0.2},
-#     )
+    measurement_input = PauliZProductInput(1, False)
+    z_basis_index = measurement_input.add_pauliz_product(
+        "ro_z",
+        [
+            0,
+        ],
+    )
+    x_basis_index = measurement_input.add_pauliz_product(
+        "ro_x",
+        [
+            0,
+        ],
+    )
+    measurement_input.add_linear_exp_val(
+        "<H>",
+        {x_basis_index: 0.1, z_basis_index: 0.2},
+    )
 
-#     measurement = PauliZProduct(
-#         constant_circuit=init_circuit,
-#         circuits=[z_circuit, x_circuit],
-#         input=measurement_input,
-#     )
+    measurement = PauliZProduct(
+        constant_circuit=init_circuit,
+        circuits=[z_circuit, x_circuit],
+        input=measurement_input,
+    )
 
-#     program = QuantumProgram(
-#         measurement=measurement,
-#         input_parameter_names=["angle_0", "angle_1"],
-#     )
+    program = QuantumProgram(
+        measurement=measurement,
+        input_parameter_names=["angle_0", "angle_1"],
+    )
 
-#     res = backend.run_program(
-#         program=program,
-#         params_values=[[0.785, 0.238], [0.234, 0.653], [0.875, 0.612]],
-#     )
+    res = backend.run_program(
+        program=program,
+        params_values=[[0.785, 0.238], [0.234, 0.653], [0.875, 0.612]],
+    )
 
-#     assert len(res) == 3
-#     for el in res:
-#         assert float(el["<H>"])
+    assert len(res) == 3
+    for el in res:
+        assert float(el["<H>"])
 
-#     init_circuit += ops.DefinitionBit("ro", 1, True)
-#     init_circuit += ops.PragmaRepeatedMeasurement("ro", 1000, None)
+    init_circuit += ops.DefinitionBit("ro", 1, True)
+    init_circuit += ops.PragmaRepeatedMeasurement("ro", 1000, None)
 
-#     measurement = ClassicalRegister(constant_circuit=None, circuits=[init_circuit, init_circuit])
+    measurement = ClassicalRegister(constant_circuit=None, circuits=[init_circuit, init_circuit])
 
-#     program = QuantumProgram(measurement=measurement, input_parameter_names=["angle_0", "angle_1"])
+    program = QuantumProgram(measurement=measurement, input_parameter_names=["angle_0", "angle_1"])
 
-#     res = backend.run_program(
-#         program=program,
-#         params_values=[[0.785, 0.238], [0.234, 0.653], [0.875, 0.612]],
-#     )
+    res = backend.run_program(
+        program=program,
+        params_values=[[0.785, 0.238], [0.234, 0.653], [0.875, 0.612]],
+    )
 
-#     assert len(res) == 3
-#     assert res[0][0]
-#     assert not res[0][1]
-#     assert not res[0][2]
+    assert len(res) == 3
+    assert res[0][0]
+    assert not res[0][1]
+    assert not res[0][2]
 
-#     measurement = ClassicalRegister(
-#         constant_circuit=None,
-#         circuits=[z_circuit, x_circuit],
-#     )
-#     program = QuantumProgram(measurement=measurement, input_parameter_names=[])
-#     res = backend.run_program(program=program, params_values=[])
+    measurement = ClassicalRegister(
+        constant_circuit=None,
+        circuits=[z_circuit, x_circuit],
+    )
+    program = QuantumProgram(measurement=measurement, input_parameter_names=[])
+    res = backend.run_program(program=program, params_values=[])
 
-#     assert len(res) == 1
-#     assert res[0][0]["ro_z"]
-#     assert res[0][0]["ro_x"]
-#     assert not res[0][1]
-#     assert not res[0][2]
+    assert len(res) == 1
+    assert res[0][0]["ro_z"]
+    assert res[0][0]["ro_x"]
+    assert not res[0][1]
+    assert not res[0][2]
 
-#     measurement = PauliZProduct(
-#         constant_circuit=None,
-#         circuits=[z_circuit, x_circuit],
-#         input=measurement_input,
-#     )
-#     program = QuantumProgram(measurement=measurement, input_parameter_names=[])
-#     res = backend.run_program(program=program, params_values=[])
+    measurement = PauliZProduct(
+        constant_circuit=None,
+        circuits=[z_circuit, x_circuit],
+        input=measurement_input,
+    )
+    program = QuantumProgram(measurement=measurement, input_parameter_names=[])
+    res = backend.run_program(program=program, params_values=[])
 
-#     assert len(res) == 1
-#     assert float(res[0]["<H>"])
+    assert len(res) == 1
+    assert float(res[0]["<H>"])
 
 
 @pytest.mark.parametrize(
@@ -570,179 +570,179 @@ def test_run_circuit_results(operations: List[Any]) -> None:
     circuit_3 += ops.PragmaGetDensityMatrix("ri", None)
 
 
-# @pytest.mark.parametrize(
-#     "operations",
-#     [
-#         [
-#             ops.PauliX(1),
-#             ops.PauliX(0),
-#             ops.PauliZ(2),
-#             ops.PauliX(3),
-#             ops.PauliY(4),
-#         ],
-#         [
-#             ops.Hadamard(0),
-#             ops.CNOT(0, 1),
-#             ops.CNOT(1, 2),
-#             ops.CNOT(2, 3),
-#             ops.CNOT(3, 4),
-#         ],
-#         [ops.RotateX(0, 0.23), ops.RotateY(1, 0.12), ops.RotateZ(2, 0.34)],
-#     ],
-# )
-# def test_measurement_register_classicalregister(operations: List[Any]) -> None:
-#     """Test QoqoTketBackend.run_measurement_registers method classical registers."""
-#     backend = QoqoTketBackend()
+@pytest.mark.parametrize(
+    "operations",
+    [
+        [
+            ops.PauliX(1),
+            ops.PauliX(0),
+            ops.PauliZ(2),
+            ops.PauliX(3),
+            ops.PauliY(4),
+        ],
+        [
+            ops.Hadamard(0),
+            ops.CNOT(0, 1),
+            ops.CNOT(1, 2),
+            ops.CNOT(2, 3),
+            ops.CNOT(3, 4),
+        ],
+        [ops.RotateX(0, 0.23), ops.RotateY(1, 0.12), ops.RotateZ(2, 0.34)],
+    ],
+)
+def test_measurement_register_classicalregister(operations: List[Any]) -> None:
+    """Test QoqoTketBackend.run_measurement_registers method classical registers."""
+    backend = QoqoTketBackend()
 
-#     circuit = Circuit()
-#     involved_qubits = set()
-#     for op in operations:
-#         involved_qubits.update(op.involved_qubits())
-#         circuit += op
+    circuit = Circuit()
+    involved_qubits = set()
+    for op in operations:
+        involved_qubits.update(op.involved_qubits())
+        circuit += op
 
-#     circuit += ops.DefinitionBit("ri", len(involved_qubits), True)
-#     circuit += ops.PragmaRepeatedMeasurement("ri", 10)
+    circuit += ops.DefinitionBit("ri", len(involved_qubits), True)
+    circuit += ops.PragmaRepeatedMeasurement("ri", 10)
 
-#     measurement = ClassicalRegister(constant_circuit=None, circuits=[circuit])
+    measurement = ClassicalRegister(constant_circuit=None, circuits=[circuit])
 
-#     try:
-#         output = backend.run_measurement_registers(measurement=measurement)
-#     except Exception:
-#         AssertionError()
+    try:
+        output = backend.run_measurement_registers(measurement=measurement)
+    except Exception:
+        AssertionError()
 
-#     assert output[0]["ri"]
-#     assert len(output[0]["ri"][0]) == len(involved_qubits)
-#     assert not output[1]
-#     assert not output[2]
-
-
-# @pytest.mark.parametrize(
-#     "operations",
-#     [
-#         [
-#             ops.PauliX(1),
-#             ops.PauliX(0),
-#             ops.PauliZ(2),
-#             ops.PauliX(3),
-#             ops.PauliY(4),
-#         ],
-#         [
-#             ops.Hadamard(0),
-#             ops.CNOT(0, 1),
-#             ops.CNOT(1, 2),
-#             ops.CNOT(2, 3),
-#             ops.CNOT(3, 4),
-#         ],
-#         [ops.RotateX(0, 0.23), ops.RotateY(1, 0.12), ops.RotateZ(2, 0.34)],
-#     ],
-# )
-# def test_measurement(operations: List[Any]) -> None:
-#     """Test QoqoTketBackend.run_measurement method."""
-#     backend = QoqoTketBackend()
-
-#     circuit = Circuit()
-#     involved_qubits = set()
-#     for op in operations:
-#         involved_qubits.update(op.involved_qubits())
-#         circuit += op
-
-#     circuit += ops.DefinitionBit("ri", len(involved_qubits), True)
-#     circuit += ops.PragmaRepeatedMeasurement("ri", 10)
-
-#     pzpinput = PauliZProductInput(number_qubits=len(involved_qubits), use_flipped_measurement=True)
-
-#     measurement = PauliZProduct(constant_circuit=None, circuits=[circuit], input=pzpinput)
-
-#     try:
-#         _ = backend.run_measurement(measurement=measurement)
-#     except Exception:
-#         AssertionError()
+    assert output[0]["ri"]
+    assert len(output[0]["ri"][0]) == len(involved_qubits)
+    assert not output[1]
+    assert not output[2]
 
 
-# @pytest.mark.parametrize(
-#     "operations",
-#     [
-#         [
-#             ops.PauliX(1),
-#             ops.PauliX(0),
-#             ops.PauliZ(2),
-#             ops.PauliX(3),
-#             ops.PauliY(4),
-#         ],
-#         [
-#             ops.Hadamard(0),
-#             ops.CNOT(0, 1),
-#             ops.CNOT(1, 2),
-#             ops.CNOT(2, 3),
-#             ops.CNOT(3, 4),
-#         ],
-#         [ops.RotateX(0, 0.23), ops.RotateY(1, 0.12), ops.RotateZ(2, 0.34)],
-#     ],
-# )
-# def test_measurement_register_statevector(operations: List[Any]) -> None:
-#     """Test QoqoTketBackend.run_measurement_registers method classical registers."""
-#     backend = QoqoTketBackend(ProjectQBackend())
+@pytest.mark.parametrize(
+    "operations",
+    [
+        [
+            ops.PauliX(1),
+            ops.PauliX(0),
+            ops.PauliZ(2),
+            ops.PauliX(3),
+            ops.PauliY(4),
+        ],
+        [
+            ops.Hadamard(0),
+            ops.CNOT(0, 1),
+            ops.CNOT(1, 2),
+            ops.CNOT(2, 3),
+            ops.CNOT(3, 4),
+        ],
+        [ops.RotateX(0, 0.23), ops.RotateY(1, 0.12), ops.RotateZ(2, 0.34)],
+    ],
+)
+def test_measurement(operations: List[Any]) -> None:
+    """Test QoqoTketBackend.run_measurement method."""
+    backend = QoqoTketBackend()
 
-#     circuit = Circuit()
-#     involved_qubits = set()
-#     for op in operations:
-#         involved_qubits.update(op.involved_qubits())
-#         circuit += op
+    circuit = Circuit()
+    involved_qubits = set()
+    for op in operations:
+        involved_qubits.update(op.involved_qubits())
+        circuit += op
 
-#     circuit += ops.DefinitionComplex("ri", len(involved_qubits), True)
-#     circuit += ops.PragmaGetStateVector("ri", None)
+    circuit += ops.DefinitionBit("ri", len(involved_qubits), True)
+    circuit += ops.PragmaRepeatedMeasurement("ri", 10)
 
-#     measurement = ClassicalRegister(constant_circuit=None, circuits=[circuit])
+    pzpinput = PauliZProductInput(number_qubits=len(involved_qubits), use_flipped_measurement=True)
 
-#     try:
-#         output = backend.run_measurement_registers(measurement=measurement)
-#     except Exception:
-#         AssertionError()
+    measurement = PauliZProduct(constant_circuit=None, circuits=[circuit], input=pzpinput)
 
-#     assert not output[0]
-#     assert not output[1]
-#     assert output[2]["ri"]
-#     assert len(output[2]["ri"][0]) == 2 ** len(involved_qubits)
+    try:
+        _ = backend.run_measurement(measurement=measurement)
+    except Exception:
+        AssertionError()
 
 
-# @pytest.mark.parametrize(
-#     "operations",
-#     [
-#         [
-#             ops.PauliX(1),
-#             ops.PauliX(0),
-#             ops.PauliZ(2),
-#             ops.PauliX(3),
-#             ops.PauliY(4),
-#         ],
-#         [
-#             ops.Hadamard(0),
-#             ops.CNOT(0, 1),
-#             ops.CNOT(1, 2),
-#             ops.CNOT(2, 3),
-#             ops.CNOT(3, 4),
-#         ],
-#         [ops.RotateX(0, 0.23), ops.RotateY(1, 0.12), ops.RotateZ(2, 0.34)],
-#     ],
-# )
-# def test_measurement_statevector(operations: List[Any]) -> None:
-#     """Test QoqoTketBackend.run_measurement method."""
-#     backend = QoqoTketBackend(ProjectQBackend())
+@pytest.mark.parametrize(
+    "operations",
+    [
+        [
+            ops.PauliX(1),
+            ops.PauliX(0),
+            ops.PauliZ(2),
+            ops.PauliX(3),
+            ops.PauliY(4),
+        ],
+        [
+            ops.Hadamard(0),
+            ops.CNOT(0, 1),
+            ops.CNOT(1, 2),
+            ops.CNOT(2, 3),
+            ops.CNOT(3, 4),
+        ],
+        [ops.RotateX(0, 0.23), ops.RotateY(1, 0.12), ops.RotateZ(2, 0.34)],
+    ],
+)
+def test_measurement_register_statevector(operations: List[Any]) -> None:
+    """Test QoqoTketBackend.run_measurement_registers method classical registers."""
+    backend = QoqoTketBackend(ProjectQBackend())
 
-#     circuit = Circuit()
-#     involved_qubits = set()
-#     for op in operations:
-#         involved_qubits.update(op.involved_qubits())
-#         circuit += op
+    circuit = Circuit()
+    involved_qubits = set()
+    for op in operations:
+        involved_qubits.update(op.involved_qubits())
+        circuit += op
 
-#     circuit += ops.DefinitionComplex("ri", len(involved_qubits), True)
-#     circuit += ops.PragmaGetStateVector("ri", None)
+    circuit += ops.DefinitionComplex("ri", len(involved_qubits), True)
+    circuit += ops.PragmaGetStateVector("ri", None)
 
-#     pzpinput = PauliZProductInput(number_qubits=len(involved_qubits), use_flipped_measurement=True)
+    measurement = ClassicalRegister(constant_circuit=None, circuits=[circuit])
 
-#     measurement = PauliZProduct(constant_circuit=None, circuits=[circuit], input=pzpinput)
+    try:
+        output = backend.run_measurement_registers(measurement=measurement)
+    except Exception:
+        AssertionError()
 
-#     try:
-#         _ = backend.run_measurement(measurement=measurement)
-#     except Exception:
-#         AssertionError()
+    assert not output[0]
+    assert not output[1]
+    assert output[2]["ri"]
+    assert len(output[2]["ri"][0]) == 2 ** len(involved_qubits)
+
+
+@pytest.mark.parametrize(
+    "operations",
+    [
+        [
+            ops.PauliX(1),
+            ops.PauliX(0),
+            ops.PauliZ(2),
+            ops.PauliX(3),
+            ops.PauliY(4),
+        ],
+        [
+            ops.Hadamard(0),
+            ops.CNOT(0, 1),
+            ops.CNOT(1, 2),
+            ops.CNOT(2, 3),
+            ops.CNOT(3, 4),
+        ],
+        [ops.RotateX(0, 0.23), ops.RotateY(1, 0.12), ops.RotateZ(2, 0.34)],
+    ],
+)
+def test_measurement_statevector(operations: List[Any]) -> None:
+    """Test QoqoTketBackend.run_measurement method."""
+    backend = QoqoTketBackend(ProjectQBackend())
+
+    circuit = Circuit()
+    involved_qubits = set()
+    for op in operations:
+        involved_qubits.update(op.involved_qubits())
+        circuit += op
+
+    circuit += ops.DefinitionComplex("ri", len(involved_qubits), True)
+    circuit += ops.PragmaGetStateVector("ri", None)
+
+    pzpinput = PauliZProductInput(number_qubits=len(involved_qubits), use_flipped_measurement=True)
+
+    measurement = PauliZProduct(constant_circuit=None, circuits=[circuit], input=pzpinput)
+
+    try:
+        _ = backend.run_measurement(measurement=measurement)
+    except Exception:
+        AssertionError()
